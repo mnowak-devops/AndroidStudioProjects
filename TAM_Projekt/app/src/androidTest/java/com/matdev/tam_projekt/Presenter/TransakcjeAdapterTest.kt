0d5c51c77fb98a3dc2bc4b494b4dd2e3f347e126
mnowak-devops <50176019+mnowak-devops@users.noreply.github.com>
2022-02-17 21:47:35 +0100

    CI

---
+++ TAM_Projekt/app/src/androidTest/java/com/matdev/tam_projekt/Presenter/TransakcjeAdapterTest.kt
@@ -0,0 +1,29 @@
+package com.matdev.tam_projekt.Presenter
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class TransakcjeAdapterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun onCreateViewHolder() {
+    }
+
+    @Test
+    fun onBindViewHolder() {
+    }
+
+    @Test
+    fun getItemCount() {
+    }
+
+    @Test
+    fun setData() {
+    }
+}
---
+++ TAM_Projekt/app/src/test/java/com/matdev/tam_projekt/Presenter/TransakcjeAdapterTest.kt
@@ -0,0 +1,22 @@
+package com.matdev.tam_projekt.Presenter
+
+import org.junit.Test
+
+class TransakcjeAdapterTest {
+
+    @Test
+    fun onCreateViewHolder() {
+    }
+
+    @Test
+    fun onBindViewHolder() {
+    }
+
+    @Test
+    fun getItemCount() {
+    }
+
+    @Test
+    fun setData() {
+    }
+}
